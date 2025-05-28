511175ff339a0f8add1954b767de216c5d32b149
omerc7 <omerc7@gmail.com>
2025-05-28 12:02:26 +0300

    new slack knowledge

--- tools/knowledge_slack.py
+++ tools/knowledge_slack.py
@@ -5,7 +5,12 @@ from kubiya_sdk.tools import function_tool
 
 @function_tool(
     description="answers questions based on the slack channel history",
-    requirements=["litellm==1.71.1", "requests==2.32.3", "pydantic==2.11.5"],
+    requirements=[
+        "litellm==1.71.1",
+        "requests==2.32.3",
+        "pydantic==2.11.5",
+        "slack-sdk==3.35.0",
+    ],
     env=[
         "LLM_BASE_URL",
         "SLACK_DOMAIN",
@@ -14,18 +19,54 @@ from kubiya_sdk.tools import function_tool
         "SLACK_CHANNEL_ID",
         "KUBIYA_USER_EMAIL",
         "KUBIYA_USER_MESSAGE",
+        "TEST_SLACK_CHANNEL_ID",  # TODO: remove this
+        "TEST_SLACK_THREAD_TS",  # TODO: remove this
     ],
-    secrets=["LLM_API_KEY", "KUBIYA_API_KEY"],
+    secrets=[
+        "LLM_API_KEY",
+        "KUBIYA_API_KEY",
+        "SLACK_API_TOKEN",
+        "TEST_SLACK_API_TOKEN",  # TODO: remove this
+    ],
 )
 def slack_knowledge():
     import os
     import json
     import litellm
     import requests
-    from pydantic import BaseModel
 
-    class Question(BaseModel):
-        question: str
+    from pydantic import BaseModel
+    from slack_sdk import WebClient
+
+    # TODO: remove this
+    os.environ["SLACK_CHANNEL_ID"] = os.environ["TEST_SLACK_CHANNEL_ID"]
+    os.environ["SLACK_THREAD_TS"] = os.environ["TEST_SLACK_THREAD_TS"]
+    os.environ["SLACK_API_TOKEN"] = os.environ["TEST_SLACK_API_TOKEN"]
+
+    class SlackMessage(BaseModel):
+        ts: str
+        user: str
+        text: str
+        thread_ts: str | None = None
+        channel_id: str
+
+    class SlackMessageKnowledgeMD(BaseModel):
+        ts: str
+        channel_id: str
+        thread_ts: str | None = None
+
+    class SlackThreadedMessageKnowledge(BaseModel):
+        content: str
+        metadata: SlackMessageKnowledgeMD
+
+    class SlackMessageKnowledge(BaseModel):
+        content: str
+        metadata: SlackMessageKnowledgeMD
+        thread: list[SlackThreadedMessageKnowledge]
+
+    class ThreadResponse(BaseModel):
+        answer: str | None = None
+        question: str | None = None
 
     class AnswerReference(BaseModel):
         ts: str
@@ -35,13 +76,28 @@ def slack_knowledge():
         content: str
         references: list[AnswerReference]
 
-    def format_slack_threads(messages):
+    def get_thread_messages(channel_id: str, ts: str) -> list[SlackMessage]:
+        client = WebClient(token=os.environ["SLACK_API_TOKEN"])
+        response = client.conversations_replies(ts=ts, channel=channel_id, limit=100)
+
+        return [SlackMessage(**m, channel_id=channel_id) for m in response["messages"]]
+
+    def format_slack_thread(messages: list[SlackMessage]):
+        formatted = "Message Thread:\n"
+        for msg in messages:
+            formatted_msg = f"User: {msg.user} Text: {msg.text}\n"
+
+            formatted += formatted_msg
+
+        return formatted
+
+    def format_slack_threads(messages: list[SlackMessageKnowledge]):
         formatted = []
         for msg in messages:
-            main_content = msg["content"].strip()
-            metadata = msg.get("metadata", {})
-            channel_id = metadata.get("channel_id", "N/A")
-            ts = metadata.get("ts", "N/A")
+            main_content = msg.content.strip()
+            metadata = msg.metadata
+            channel_id = metadata.channel_id
+            ts = metadata.ts
 
             formatted_msg = (
                 f"Main Message:\n"
@@ -49,14 +105,13 @@ def slack_knowledge():
                 f"(channel_id: {channel_id}, ts: {ts})"
             )
 
-            thread_replies = msg.get("thread", [])
+            thread_replies = msg.thread
             if thread_replies:
                 formatted_msg += "\n\nThread Replies:"
                 for reply in thread_replies:
-                    reply_content = reply["content"].strip()
-                    reply_metadata = reply.get("metadata", {})
-                    reply_channel_id = reply_metadata.get("channel_id", "N/A")
-                    reply_ts = reply_metadata.get("ts", "N/A")
+                    reply_content = reply.content.strip()
+                    reply_channel_id = reply.metadata.channel_id
+                    reply_ts = reply.metadata.ts
                     formatted_msg += (
                         f"\n  - {reply_content} "
                         f"(Channel ID: {reply_channel_id}, Timestamp: {reply_ts})"
@@ -66,30 +121,26 @@ def slack_knowledge():
 
         return "\n\n---\n\n".join(formatted)
 
-    def query_rag(query: str):
-        try:
-            kubiya_api_url = os.environ["KUBIYA_API_URL"]
-            payload = {
-                "threshold": 0.5,
-                "query": query,
-                # "channel_id": os.environ["SLACK_CHANNEL_ID"], # TODO: add channel id
-            }
-            headers = {
-                "Authorization": f"UserKey {os.environ['KUBIYA_API_KEY']}",
-            }
-            result = requests.post(f"{kubiya_api_url}/api/v1/rag/query/mind", json=payload, headers=headers)
-            result.raise_for_status()
+    def query_rag(query: str, channel_id: str) -> list[SlackMessageKnowledge]:
+        kubiya_api_url = os.environ["KUBIYA_API_URL"]
+        payload = {
+            "threshold": 0.5,
+            "query": query,
+            "channel_id": channel_id,
+        }
+        headers = {
+            "Authorization": f"UserKey {os.environ['KUBIYA_API_KEY']}",
+        }
+        result = requests.post(
+            f"{kubiya_api_url}/api/v1/rag/query/slack", json=payload, headers=headers
+        )
+        result.raise_for_status()
 
-            return result.json()
-        except Exception as e:
-            print(e)
-            print("tool ended with error")
-            return
+        return [SlackMessageKnowledge(**msg) for msg in result.json()]
 
     def pretty_print_answer(answer: Answer):
         slack_domain = os.environ["SLACK_DOMAIN"]
         print(f"Answer: {answer.content}")
-        # TODO: add references with slack links
         print("\n  References:")
         for ref in answer.references:
             print(
@@ -100,31 +151,40 @@ def slack_knowledge():
     llm_base_url = os.environ["LLM_BASE_URL"]
 
     try:
+        thread_messages = get_thread_messages(
+            os.environ["SLACK_CHANNEL_ID"], os.environ["SLACK_THREAD_TS"]
+        )
+        thread_context = format_slack_thread(thread_messages)
         response = litellm.completion(
             model="openai/gpt-4o",
             api_key=llm_key,
             base_url=llm_base_url,
-            response_format=Question,
+            response_format=ThreadResponse,
             messages=[
                 {
-                    "content": "Take the user's query and expand it into a more detailed and informative version to help retrieve better results. "
-                    "Add assumptions, context, and related keywords if needed. Be concise but specific. Return the expanded query only! No other text.",
+                    "content": "Your task is to understand the user's question based on the messages in the thread. If the answer is in the thread, just return the answer. If the answer is not in the thread, return a concisequestion that will help retrieve better results.",
                     "role": "system",
                 },
                 {
-                    "content": f"User message: {os.environ['KUBIYA_USER_MESSAGE']}",
+                    "content": f"Thread context: {thread_context}\n\nUser message: {os.environ['KUBIYA_USER_MESSAGE']}",
                     "role": "user",
                 },
             ],
         )
 
-        expended_query = Question(**json.loads(response.choices[0].message.content)).question
+        thread_res = ThreadResponse(**json.loads(response.choices[0].message.content))
 
-        result = query_rag(
-            expended_query
-        )
-        messages_with_thread_replies = [m for m in result if m.get("thread")]
-        formated_result = format_slack_threads(messages_with_thread_replies)
+        if thread_res.answer:
+            print(f"Answer (based on the thread): {thread_res.answer}")
+            return
+
+        if not thread_res.question:
+            print("Question could not be extracted from the thread")
+            return
+
+        result = query_rag(thread_res.question, os.environ["SLACK_CHANNEL_ID"])
+
+        formated_result = format_slack_threads(result)
 
         response = litellm.completion(
             model="openai/gpt-4o",
